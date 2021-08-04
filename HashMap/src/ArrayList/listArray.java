3665b876f40b32e273947fca3dcbdffe0a97809d
AriyanAyan <ariyanayan07@gmail.com>
2021-08-04 02:28:52 -0400

    new code is added

---
+++ HashMap/src/ArrayList/listArray.java
@@ -0,0 +1,15 @@
+package ArrayList;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class listArray {
+
+	public static void main(String[] args) 
+	
+	{	
+		String ar[] = {"Mamun","Chowdhury","Women","Dog"};
+		List arr = Arrays.asList(ar);
+		System.out.println(arr);
+	}
+}
